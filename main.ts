2e3a64d9d0b3a998771cad8be03f3f49505d61a0
Shane Lee <shanelee007@gmail.com>
2022-04-22 17:39:13 +1000

    ec2 example

--- main.ts
+++ main.ts
@@ -1,31 +1,68 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { Container, Image, DockerProvider } from "@cdktf/provider-docker";
+// import { Container, Image, DockerProvider } from "@cdktf/provider-docker";
+import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { AwsProvider, ec2 } from "@cdktf/provider-aws";
 
 class MyStack extends TerraformStack {
-	constructor(scope: Construct, name: string) {
-		super(scope, name);
+	constructor(scope: Construct, id: string) {
+		super(scope, id);
 
-		new DockerProvider(this, "default", {});
-
-		const dockerImage = new Image(this, "nginxImage", {
-			name: "nginx:latest",
-			keepLocally: false,
+		new AwsProvider(this, "AWS", {
+			region: "us-west-1",
+      profile: "shane"
 		});
 
-		new Container(this, "nginxContainer", {
-			image: dockerImage.latest,
-			name: "tutorial",
-			ports: [
-				{
-					internal: 80,
-					external: 8000,
-				},
-			],
+		const instance = new ec2.Instance(this, "compute", {
+			ami: "ami-01456a894f71116f2",
+			instanceType: "t2.micro",
+      tags: {
+        Name: "shane-cdktf-demo"
+      }
+		});
+
+		new TerraformOutput(this, "public_ip", {
+			value: instance.publicIp,
 		});
 	}
 }
 
 const app = new App();
-new MyStack(app, "typescript-docker");
+new MyStack(app, "aws_instance");
+
+// new RemoteBackend(stack, {
+// 	hostname: "app.terraform.io",
+// 	organization: "<YOUR_ORG>",
+// 	workspaces: {
+// 		name: "learn-cdktf",
+// 	},
+// });
+
 app.synth();
+
+// class MyStack extends TerraformStack {
+// 	constructor(scope: Construct, name: string) {
+// 		super(scope, name);
+
+// 		new DockerProvider(this, "default", {});
+
+// 		const dockerImage = new Image(this, "nginxImage", {
+// 			name: "nginx:latest",
+// 			keepLocally: false,
+// 		});
+
+// 		new Container(this, "nginxContainer", {
+// 			image: dockerImage.latest,
+// 			name: "tutorial",
+// 			ports: [
+// 				{
+// 					internal: 80,
+// 					external: 8000,
+// 				},
+// 			],
+// 		});
+// 	}
+// }
+
+// const app = new App();
+// new MyStack(app, "typescript-docker");
+// app.synth();
